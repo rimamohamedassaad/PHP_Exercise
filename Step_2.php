453eb079759b33b3b96f2b8085ff8ebca8a50fd0
rimamohamedassaad <rimamohamadassaad@gmail.com>
2022-02-24 13:58:37 +0200

    Step 2 : Echo & Print

---
+++ Step_2.php
@@ -0,0 +1,6 @@
+Hello world, Help me I am using PHP
+<?php 
+// I am getting errors but I can't see the errors
+echo " I am getting errors but I can't see the errors";
+print("Search on how to enable PHP Errors")
+?>
