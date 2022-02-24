fc50956b7cb082e6ee07eb3f4d06acb451100f8d
rimamohamedassaad <rimamohamadassaad@gmail.com>
2022-02-24 14:10:23 +0200

    Step 3 : Variables lvl 1

---
+++ Step_3.php
@@ -0,0 +1,11 @@
+<?php 
+
+$name = 'rima';
+$email = 'rimamohamadassaad@gmail.com';
+echo "my name is $name and my email is $email";
+$height = 156;
+$width = 22;
+$area = $height * $width;
+echo "the area is $area"
+
+?>
