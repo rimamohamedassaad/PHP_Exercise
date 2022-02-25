466d14fff384c505756e332fc84a0022caa53d56
rimamohamedassaad <rimamohamadassaad@gmail.com>
2022-02-25 14:05:51 +0200

    Step 7 :PHP Control Statements -  If / else

---
+++ Step_7.php
@@ -0,0 +1,10 @@
+<?php
+$birth_year = 2010;
+$current_year = 2022;
+if( $current_year - $birth_year > 18){
+    echo " you can drive";
+}
+else {
+    echo "You still a kid, go and play GTA";
+}
+?>
