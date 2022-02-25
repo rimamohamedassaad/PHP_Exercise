c36ad4e3c11a87c05f9ae8222a8d61c0810736a3
rimamohamedassaad <rimamohamadassaad@gmail.com>
2022-02-25 13:40:40 +0200

    Step 5 : Data types - lvl 1

---
+++ Step_5.php
@@ -0,0 +1,18 @@
+<?php
+$course_name = "PHP";
+$enrolled_students = 21;
+$price = 99.99;
+$on_discount=true;
+echo "course name : $course_name 
+enrolled_students : $enrolled_students 
+course price : $price $ USD \n";
+if($on_discount == true)
+{
+  echo "Course on discount: Yes";
+
+}
+else {
+  echo "Course on discount: no";
+}
+
+?> 
