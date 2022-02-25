2aa14107ee827ed36e6c8493d1c58c8dc3bdee6e
rimamohamedassaad <rimamohamadassaad@gmail.com>
2022-02-25 12:34:14 +0200

    Step 4 : Strings Concatenation

---
+++ Step_4.php
@@ -0,0 +1,8 @@
+<?php
+$name="rima";
+$position= "full-stack developer";
+$github_url= "https://github.com/rimamohamedassaad";
+echo "Hello, I'm " . "$name" . " I'm a " .  "$position". " please check my github link" . "$github_url" ;
+echo "Hello, I'm  $name I'm a  $position  please check my github link $github_url" ;
+echo 'Hello, I\' m' . $name . 'I\'m' . $position . 'please check my github link'  . $github_url; 
+?> 
