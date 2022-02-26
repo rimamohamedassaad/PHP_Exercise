aa6d385c1944a767ff41dcf738c04ad67d8ba9cf
rimamohamedassaad <rimamohamadassaad@gmail.com>
2022-02-26 22:06:04 +0200

    basic_2 Step 3 function

--- basic_2/Step_3.php
+++ basic_2/Step_3.php
@@ -1,6 +1,6 @@
 <?php
-$var1 = 32;
-$var2 = 45;
+// $var1 = 32;
+// $var2 = 45;
 //first method reassign 
 //  $var1 = 45;
 //  $var2 = 32;
@@ -8,11 +8,14 @@ $var2 = 45;
 // echo "$var1" ,
 // "$var2";
 //second way
-
-$x = $var1;
-$var1 = $var2;
-$var2 = $x;
-echo "$var1" ,
-"$var2";
+function swap($var1,$var2){
+    $x = $var1;
+    $var1 = $var2;
+    $var2 = $x;
+    echo "$var1" ,
+    "$var2";
+    
+}
+swap(32,45);
 
 ?>
