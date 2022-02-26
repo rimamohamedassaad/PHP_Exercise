7a8d3eb92e82ade8f8b1f424460804c85b286b66
rimamohamedassaad <rimamohamadassaad@gmail.com>
2022-02-26 20:21:05 +0200

    basic_2 Step 7

---
+++ basic_2/Step_7.php
@@ -0,0 +1,13 @@
+<?php
+// $string = "Hello i'm rima and my favorite programing languge is paython";
+// $find = "paython";
+// $replace= "php";
+// echo str_replace($find,$replace,$string);
+function streplace($string)
+{
+$find = "paython";
+$replace= "php";
+echo str_replace($find,$replace,$string);
+}
+streplace("Hello i'm rima and my favorite programing languge is paython");
+?>
