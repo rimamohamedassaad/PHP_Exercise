08abdd4be4d4545f86e1dd2c7f0de7cd7d96ba17
rimamohamedassaad <rimamohamadassaad@gmail.com>
2022-02-27 20:41:39 +0200

    ex1 using while

--- basic_3/Step_1.php
+++ basic_3/Step_1.php
@@ -1,25 +1,35 @@
 <?php
 function stars(){
-    for($i=1; $i<=8; $i++) {
-        for($j=1; $j<=$i; $j++)
-        {
-        echo '*';
+    // for($i=1; $i<=8; $i++) {
+    //     for($j=1; $j<=$i; $j++)
+    //     {
+    //     echo '*';
+    //     }
+    //     echo "\n";
+    //     }
+    while($i <= 8){
+        $j=1;
+        while($j<$i){
+            $j++;
+            echo "*";
+            
         }
         echo "\n";
-        }
+        $i++;
+    }
 }
-stars();
-function usestars($num1){
-    for($i=1; $i<=$num1; $i++) {
-        for($j=1; $j<=$i; $j++)
-        {
-        echo '*';
-        }
-        echo "\n";
-        }
-}
+// stars();
+// function usestars($num1){
+//     for($i=1; $i<=$num1; $i++) {
+//         for($j=1; $j<=$i; $j++)
+//         {
+//         echo '*';
+//         }
+//         echo "\n";
+//         }
+// }
 
-usestars(4);
-
+// usestars(4);
+echo stars();
 
 ?>
