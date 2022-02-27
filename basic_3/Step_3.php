66bb87320b497d6ffafc8dd9eb8c7e7476bebe97
rimamohamedassaad <rimamohamadassaad@gmail.com>
2022-02-27 12:06:10 +0200

    basic_3 Step 3 while loop

--- basic_3/Step_3.php
+++ basic_3/Step_3.php
@@ -1,9 +1,26 @@
 <?php
-function find($word,$text){
-$arr = explode($word,$text);
+// function find($word,$text){
+// $arr = explode($word,$text);
 
-$length = count($arr) -1;
-echo $length;
+// $length = count($arr) -1;
+// echo $length;
+// }
+function find($word,$text){
+    $arr = explode(" ",$text);
+    $length= count($arr);
+    $i=0;
+    $sum =0;
+    while($i< $length){
+if($arr[$i]== $word){
+    $sum = $sum +1;
 }
-find("php", "i love php and iif you ask me about php, i'll answer  php is my favorite, you can cal me rima php");
+        $i++;
+    }
+return $sum;
+
+}
+
+
+
+echo find("php" , "you php cal php rima php also php also php one php");
 ?>
