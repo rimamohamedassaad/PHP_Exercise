cea63cb49a151809398064e2022f86c29c391b5b
rimamohamedassaad <rimamohamadassaad@gmail.com>
2022-02-27 14:41:51 +0200

    basic_3 loop for ex5

---
+++ basic_3/Step_8.php
@@ -0,0 +1,23 @@
+<?php 
+function reverse($text)
+{
+$arr = explode(" ",$text);
+$length = count($arr);
+$i=$length;
+$arrone;
+for($i=1;$i<=$length;$i++){
+
+    $arrone[$i]=$arr[$length -$i];
+echo "$arrone[$i] ";
+}
+
+
+// foreach($arr as $val){
+// $val = $arr[$i-1];
+// $i--;
+// echo "$val ";
+// }
+
+}
+reverse("hello my name is rima")
+?>
